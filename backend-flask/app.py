3ea54d18043a97c6bf00cba411f3b4db84e5f269
Insha Bano <inshabano88158@gmail.com>
2024-08-31 19:18:09 +0000

    aws-xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,6 +33,14 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+# AWS Xray middleware
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
