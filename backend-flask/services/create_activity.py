c73d736f203edb07531460404468209330682dc0
insha6bano <dev0ps9630@gmail.com>
2024-09-12 21:19:13 +0530

    Update create_activity.py

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,11 +1,14 @@
 import uuid
 from datetime import datetime, timedelta, timezone
 class CreateActivity:
+  def validations():
   def run(message, user_handle, ttl):
     model = {
       'errors': None,
       'data': None
     }
+   user_uuid = ''
+   
 
     now = datetime.now(timezone.utc).astimezone()
 
@@ -40,6 +43,7 @@ class CreateActivity:
         'message': message
       }   
     else:
+      self create_activity()
       model['data'] = {
         'uuid': uuid.uuid4(),
         'display_name': 'Insha Bano',
@@ -49,3 +53,18 @@ class CreateActivity:
         'expires_at': (now + ttl_offset).isoformat()
       }
     return model
+  def create_activity(user_uuid,message,expires_at):
+    sql = f"""
+    INSERT INTO {
+      user_uuid,
+      message,
+      expires_at
+    }
+    VALUES{
+      "{user_uuid}",
+      "{message}",
+      "{expires_at}"
+    
+      
+    }
+    """
