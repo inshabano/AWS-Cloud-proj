1923e6b428b2bb9fa736e0cc8b2d5807a6d5123f
insha6bano <dev0ps9630@gmail.com>
2024-09-13 10:39:09 +0530

    Update db.py

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -3,18 +3,76 @@ import os
 connection_url = os.getenv("CONNECTION_URL")
 pool = ConnectionPool(connection_url)
 
-def query_wrap_object(template):
-  sql = f"""
-  (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
-  {template}
-  ) object_row);
-  """
-  return sql
-def query_wrap_array(template):
-  sql = """
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-  {template}
-  ) array_row);
-  """
-  return sql
+class Db:
+  def __init__(self):
+    self.init_pool()
+    
+  def init_pool(self):
+    connection_url = os.getenv("CONNECTION_URL")
+    self.pool = ConnectionPool(connection_url)
+
+  def query_commit(self):
+    try:
+      with self.pool.connection() as conn:
+        cur =  conn.cursor()
+        cur.execute(sql)
+        conn.commit() 
+    except Exception as err:
+      self.print_sql_err(err)
+
+  def query_array_json(self,sql):
+    print(sql+"\n")
+    wrapped_sql = self.query.wrap_array(sql)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(wrapped_sql)
+        # this will return a tuple
+        # the first field being the data
+        json = cur.fetchone() 
+        return json[0]
+  def query_object_json(self,sql):
+    print(sql+"\n")
+    wrapped_sql = self.query_wrap_object(sql)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(wrapped_sql)
+        # this will return a tuple
+        # the first field being the data
+        json = cur.fetchone()
+        return json[0]
+    
+  def query_wrap_object(self,template): 
+    sql = f"""
+    (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+    {template}
+    ) object_row);
+    """
+    return sql
+  def query_wrap_array(self,template):
+    sql = """
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
+  def print_sql_err(self,err):
+    # get details about the exception
+    err_type, err_obj, traceback = sys.exc_info()
+  
+    # get the line number when exception occured
+    line_num = traceback.tb_lineno
+  
+    # print the connect() error
+    print ("\npsycopg ERROR:", err, "on line number:", line_num)
+    print ("psycopg traceback:", traceback, "-- type:", err_type)
+  
+    # print the pgcode and pgerror exceptions
+    print ("pgerror:", err.pgerror)
+    print ("pgcode:", err.pgcode, "\n")
+  
+
+db = Db()
+
+
+  
   
