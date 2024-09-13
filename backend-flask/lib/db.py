fb14ff5b64db3df14f399a5174f3bbc794cd87f4
insha bano <dev0ps9630@gmail.com>
2024-09-13 11:23:43 +0000

    crud

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -2,22 +2,40 @@ from psycopg_pool import ConnectionPool
 import os
 import sys
 import re
+from flask import current_app as app
 
 class Db:
+
   def __init__(self):
     self.init_pool()
+  
+  def template(self,*args):
+   
+    pathing = list((app.root_path,'db','sql',) + args)
+    pathing[-1] = pathing[-1] + ".sql"
+
+    template_path = os.path.join(*pathing)
+
+    green = '\033[92m'
+    no_color = '\033[0m'
+    print("\n")
+    print(f'{green} Load SQL Template: {template_path} {no_color}')
+
+    with open(template_path, 'r') as f:
+      template_content = f.read()
     
   def init_pool(self):
     connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
 
-  def query_commit(self,sql,**kwargs):
+  def query_commit(self,sql,params={}):
+    print(sql+"\n")
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
 
     try:
       with self.pool.connection() as conn:
-        cur =  conn.cursor()
+        cur = conn.cursor()
         cur.execute(sql,params)
         if is_returning_id:
           returning_id = cur.fetchone()[0]
@@ -27,22 +45,25 @@ class Db:
     except Exception as err:
       self.print_sql_err(err)
   
-  def query_array_json(self,sql):
-    print(sql+"\n")
+  def query_array_json(self,sql,params={}):
+    self.print_sql('array',sql)
+   
     wrapped_sql = self.query.wrap_array(sql)
+
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
-        cur.execute(wrapped_sql)
+        cur.execute(wrapped_sql,params)
         # this will return a tuple
         # the first field being the data
         json = cur.fetchone() 
         return json[0]
-  def query_object_json(self,sql):
-    print(sql+"\n")
+  def query_object_json(self,sql,params={}):
+    self.print_sql('json',sql)
+    
     wrapped_sql = self.query_wrap_object(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
-        cur.execute(wrapped_sql)
+        cur.execute(wrapped_sql,params)
         # this will return a tuple
         # the first field being the data
         json = cur.fetchone()
