24c3832d160af637163b301150ab55e06e55c769
insha bano <dev0ps9630@gmail.com>
2024-09-15 10:17:14 +0000

    SETUP DB

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,12 +1,16 @@
 import uuid
-from lib.db import db
 from datetime import datetime, timedelta, timezone
+
+from lib.db import db
+
+
 class CreateActivity:
-  def run(message, user_handle, ttl):
+  def run(message, cognito_user_id, ttl):
     model = {
       'errors': None,
       'data': None
     }
+    
     now = datetime.now(timezone.utc).astimezone()
 
     if (ttl == '30-days'):
@@ -26,8 +30,8 @@ class CreateActivity:
     else:
       model['errors'] = ['ttl_blank']
 
-    if user_handle == None or len(user_handle) < 1:
-      model['errors'] = ['user_handle_blank']
+    if cognito_user_id == None or len(cognito_user_id) < 1:
+      model['errors'] = ['cognito_user_id_blank']
 
     if message == None or len(message) < 1:
       model['errors'] = ['message_blank'] 
@@ -36,27 +40,28 @@ class CreateActivity:
 
     if model['errors']:
       model['data'] = {
-        'handle':  user_handle,
+        'cognito_user_id':  cognito_user_id,
         'message': message
       }   
     else:
       expires_at = (now + ttl_offset)
-      uuid = CreateActivity.create_activity(user_handle,message,expires_at)
+      uuid = CreateActivity.create_activity(cognito_user_id,message,expires_at)
+      
       object_json = CreateActivity.query_object_activity(uuid)
       model['data'] = object_json
     return model
-  def (handle, message, expires_at):
+  
+  def create_activity(cognito_user_id, message, expires_at):
     sql = db.template('activities','create')
     uuid = db.query_commit(sql,{ 
-      'handle': handle, 
+      'cognito_user_id': cognito_user_id, 
       'message': message, 
       'expires_at': expires_at
     })
-    return uuid
     
+  
   def query_object_activity(uuid):
-    sql = db.template('activities','object')
+    sql = db.template('activities', 'object')
     return db.query_object_json(sql,{ 
       'uuid': uuid
       })
-
